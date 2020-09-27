9b9b2e4187cc32e8a8a118e668f8603d0842718d
sohwijung <sohwi.j@gmail.com>
2020-09-27 17:36:57 -0400

    Added functionality.

---
+++ scripts/app.js
@@ -0,0 +1,55 @@
+// dom queries
+const rooms = document.querySelector('.chatrooms');
+const newRoomForm = document.querySelector('.new-chat-room');
+
+const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+
+rooms.addEventListener("click", function(e) {
+  e.target.classList.toggle("active")
+})
+
+newRoomForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const roomName = newRoomForm.message.value.trim();
+  rooms.innerHTML+= `<button class="btn btn-light ml-2" id="${roomName}">#${roomName}</button>`
+});
+
+// add a new chat
+newChatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err));
+});
+
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  newNameForm.reset();
+  updateMssg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => updateMssg.innerText = '', 3000);
+});
+
+// update the chat room
+// rooms.addEventListener('click', e => {
+//   if(e.target.tagName === 'BUTTON'){
+//     chatUI.clear();
+//     chatroom.updateRoom(e.target.getAttribute('id'));
+//     chatroom.getChats(chat => chatUI.render(chat));
+//   }
+// });
+
+// check local storage for name
+const username = localStorage.username ? localStorage.username : 'anon';
+
+// class instances
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('class', username);
+
+// get chats & render
+chatroom.getChats(data => chatUI.render(data));
